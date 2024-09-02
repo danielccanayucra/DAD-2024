4fb09e19ebf164028bc27dfca11dbf60628723b6
ccanayucra9 <ccanadaniely@gmail.com>
2024-09-02 10:28:24 -0500

    ayuda

--- ms-cliente/src/main/java/com/example/mscliente/controller/ClienteController.java
+++ ms-cliente/src/main/java/com/example/mscliente/controller/ClienteController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/cliente")
+@RequestMapping("/client")
 public class ClienteController {
     @Autowired
     private ClienteService clienteService;
